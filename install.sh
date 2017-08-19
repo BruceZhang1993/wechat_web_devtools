298e1e1e4d5ddd82889b83b48139adab6087c3b7
Bruce Zhang <zttt183525594@gmail.com>
2017-08-19 17:04:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 install_path='/opt/tencent';
 current_dir=`pwd`
 desktop_dir='/usr/share/applications'
-soft_link_path='/usr/local/bin/wechat_web_devtools'
+soft_link_path='/usr/local/bin/wechat_web_devtools_beta'
 
 # install app
 argv1=$1
@@ -15,7 +15,7 @@ if [ ! -d "$install_path" ]; then
     sudo mkdir -p $install_path
 fi
 
-app_path="${install_path}/wechat_web_devtools"
+app_path="${install_path}/wechat_web_devtools_beta"
 if [ -d $app_path ]; then
  sudo rm -rf $app_path
 fi
@@ -23,11 +23,11 @@ fi
 sudo cp -r $current_dir $app_path
 
 sed_argv="s/=\/opt\/tencent/=${install_path//'/'/'\/'}/ig"
-sudo sed -i $sed_argv "${app_path}/wechat_web_devtools.desktop"
+sudo sed -i $sed_argv "${app_path}/wechat_web_devtools_beta.desktop"
 
 sudo chown -R $USER:users $app_path
 
-cat >"${app_path}/wechat_web_devtools"<<EOF
+cat >"${app_path}/wechat_web_devtools_beta"<<EOF
 #!/bin/sh
 
 cd $app_path
@@ -37,14 +37,14 @@ EOF
 
 # create shortcut
 if [ -d "$desktop_dir" ]; then
-    sudo cp "${app_path}/wechat_web_devtools.desktop" $desktop_dir
+    sudo cp "${app_path}/wechat_web_devtools_beta.desktop" $desktop_dir
 fi
 
 # create soft link and boot application from terminal
 if [ -L $soft_link_path ]; then
     sudo rm -f $soft_link_path
 fi;
-sudo ln -s "${app_path}/wechat_web_devtools" $soft_link_path
+sudo ln -s "${app_path}/wechat_web_devtools_beta" $soft_link_path
 sudo chown $USER:$USER $soft_link_path
 
 echo "Installation Successfully!"
